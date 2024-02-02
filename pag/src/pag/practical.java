7a68f481bd0e8f908d75849329dadf5e2cf97343
sdac-40 <sdac-40@sdac-40-PC>
2024-02-02 17:07:29 +0530

    its me VS me

--- pag/src/pag/practical.java
+++ pag/src/pag/practical.java
@@ -4,5 +4,7 @@ public class practical {
   public static void main(String[] args) {
 	System.out.println("WASSUP? homies");
 	System.out.println( " hii ");
+	System.out.println(" hii mai khalifa");
+	
 }
 }
